826715fd90c16d85f19c70885b1e7b07f312f9d8
sio-maker <sio0040maker@gmai.com>
2020-12-22 15:55:30 +0900

    firebaseとの連携 ローカル上でのクラッド

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDXGtuh1cxofjvx1Htb4wG5jEd9b6PAr0",
+  authDomain: "hero2-bd472.firebaseapp.com",
+  projectId: "hero2-bd472",
+  storageBucket: "hero2-bd472.appspot.com",
+  messagingSenderId: "609758600274",
+  appId: "1:609758600274:web:faeaf09337acd7f1d7138b",
+  measurementId: "G-HYC60J2DNQ",
+};
+
+firebase.initializeApp(firebaseConfig);
--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
-import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+import "./firebase.js";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
