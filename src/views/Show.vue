321978ee550b6e08721dbc3a8481e1d4ac62e90b
sio-maker <sio0040maker@gmai.com>
2021-01-09 09:24:55 +0900

    show pageでcheckできる様にした

--- src/views/Show.vue
+++ src/views/Show.vue
@@ -2,7 +2,9 @@
   <div class="show">
     <h1>this is show page</h1>
     <h1>{{ this.$route.params["id"] }}</h1>
-    <h1>{{ this.todo.item }}</h1>
+    <input type="checkbox" v-model="todo.isDone" v-on:change="checked(todo)" />
+    <div class="done-item" v-if="todo.isDone">{{ this.todo.item }}</div>
+    <div v-else>{{ this.todo.item }}</div>
   </div>
 </template>
 <script>
@@ -31,5 +33,28 @@ export default {
         this.todo = collection.data();
       });
   },
+  methods: {
+    checked(todo) {
+      var which = firebase
+        .firestore()
+        .collection("todoA")
+        .doc(todo.id);
+
+      which.set(
+        {
+          isDone: todo.isDone,
+        },
+        {
+          merge: true,
+        }
+      );
+      console.log("Checked:", todo.isDone);
+    },
+  },
 };
 </script>
+<style>
+.done-item {
+  text-decoration: line-through;
+}
+</style>
